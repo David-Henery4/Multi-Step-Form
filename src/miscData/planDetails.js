9c5f3eb3c193c1c471bb37a69910c5e738a85017
DizzlyD <david4henery00@hotmail.com>
2023-05-25 22:34:16 +0100

    plan-items-mob-&-desk

--- src/miscData/planDetails.js
+++ src/miscData/planDetails.js
@@ -6,18 +6,24 @@ const planDetails = [
     icon: ArcadeIcon,
     title: "Arcade",
     price: "$9/mo",
+    offer: "2 months free",
+    isActive: true,
   },
   {
     id: 2,
     icon: AdvancedIcon,
     title: "Advanced",
     price: "$12/mo",
+    offer: "2 months free",
+    isActive: false,
   },
   {
     id: 3,
     icon: ProIcon,
     title: "Pro",
     price: "$15/mo",
+    offer: "2 months free",
+    isActive: false,
   },
 ];
 
--- src/pages/PlanPage.jsx
+++ src/pages/PlanPage.jsx
@@ -9,20 +9,36 @@ const PlanPage = () => {
         subTitle="You have the option of monthly or yearly billing."
       />
       <div className="plan-page-content">
-        <div>
+        <div className="plan-page-content-options">
           {planDetails.map((plans) => {
             return (
-              <div key={plans?.id}>
+              <div
+                key={plans?.id}
+                className={`${
+                  plans.isActive
+                    ? "plan-page-content-options-item plan-page-content-options-item--active"
+                    : "plan-page-content-options-item"
+                }`}
+              >
                 <div>{plans?.icon && <plans.icon />}</div>
-                <div>
-                  <h3>{plans?.title}</h3>
-                  <p>{plans.price}</p>
+                <div className="plan-page-content-options-item-text">
+                  <h3 className="plan-label-header">{plans?.title}</h3>
+                  <p className="plan-details">
+                    <span className="plan-label-price">{plans?.price}</span>
+                    <span className="step-label-offer">{plans?.offer}</span>
+                  </p>
                 </div>
               </div>
             );
           })}
         </div>
-        <div></div>
+        <div>
+          <p>Monthly</p>
+          <div className="toggle">
+            <div className="toggle__ball"></div>
+          </div>
+          <p>Yearly</p>
+        </div>
       </div>
       <div className="btn-container-desk">
         <NextPrevBtns />
