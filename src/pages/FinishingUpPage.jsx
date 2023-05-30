6702aed104083b4f35050b1d239341e7bdfb9ddd
DizzlyD <david4henery00@hotmail.com>
2023-05-30 23:40:15 +0100

    added-responsive-finishing-up-section

--- src/pages/FinishingUpPage.jsx
+++ src/pages/FinishingUpPage.jsx
@@ -1,4 +1,4 @@
-import { ContentHeaders } from "../components";
+import { ContentHeaders, NextPrevBtns } from "../components";
 
 const FinishingUpPage = () => {
   return (
@@ -8,25 +8,32 @@ const FinishingUpPage = () => {
         subTitle="Double-check everything looks OK before confirming."
       />
       <div className="finishing-up-details">
-        <div>
-          <div>
+        <div className="finishing-up-details-prices">
+          <div className="finishing-up-details-prices-plan">
             <div>
-              <h2>Arcade (Yearly)</h2>
-              <p>Change</p>
+              <h2 className="add-ons-label">Arcade (Yearly)</h2>
+              <p className="plan-label-price">Change</p>
             </div>
-            <p>$90/yr</p>
+            <p className="final-plan-price-label">$90/yr</p>
+          </div>
+          <div className="finishing-up-details-prices-addons">
+            <div>
+              <h3 className="plan-label-price">Online service</h3>
+              <p className="final-addons-price-label">+$10/yr</p>
+            </div>
+            <div>
+              <h3 className="plan-label-price">Online service</h3>
+              <p className="final-addons-price-label">+$10/yr</p>
+            </div>
           </div>
         </div>
-        <div>
-          <div>
-            <h3>Online service</h3>
-            <p>+$10/yr</p>
-          </div>
-          <div>
-            <h3>Online service</h3>
-            <p>+$10/yr</p>
-          </div>
+        <div className="finishing-up-details-total">
+          <h3 className="plan-label-price">Total (per year)</h3>
+          <p className="total-price-label">$120/yr</p>
         </div>
+      </div>
+      <div className="btn-container-desk">
+        <NextPrevBtns />
       </div>
     </div>
   );
